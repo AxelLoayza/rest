110f834b643a956bd7b645f594f8db07967621fc
AxelLoayza <axtev2000@hotmail.com>
2025-05-07 10:30:21 -0500

    ahora six4

--- tienda_ropa/tienda_ropa/wsgi.py
+++ tienda_ropa/tienda_ropa/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tienda_ropa.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tienda_ropa.tienda_ropa.settings')
 
 application = get_wsgi_application()
